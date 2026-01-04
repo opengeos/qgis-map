f210fb8432b422f84eb5b89ac3a79565ce1eaad5
Qiusheng Wu <giswqs@gmail.com>
2026-01-04 00:27:03 -0500

    Bump version: 0.0.1 → 0.1.0

--- qgis_map/__init__.py
+++ qgis_map/__init__.py
@@ -5,7 +5,7 @@ A high-level Python API for working with QGIS, inspired by leafmap.
 
 __author__ = """Qiusheng Wu"""
 __email__ = "giswqs@gmail.com"
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 
 from .qgis_map import Map
 from .basemaps import BASEMAPS, get_basemap_names, get_basemap_url, get_xyz_uri
